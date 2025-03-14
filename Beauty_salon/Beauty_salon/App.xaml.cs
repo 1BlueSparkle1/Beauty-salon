c91bb8ff190e95099333f3516eb1ae387b2fc416
LERA <LERA@WIN-FL0E7E95RSD>
2025-03-14 13:34:04 +0300

    start project

---
+++ Beauty_salon/Beauty_salon/App.xaml.cs
@@ -0,0 +1,19 @@
+﻿using Beauty_salon.Components;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Beauty_salon
+{
+    /// <summary>
+    /// Логика взаимодействия для App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        public static Beauty_salonEntities db = new Beauty_salonEntities();
+    }
+}
